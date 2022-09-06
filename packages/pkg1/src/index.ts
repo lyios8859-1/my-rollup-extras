e49a604a7f7e1a68afbf050b7429c5e6715aa7f4
Timly <liu67839@gmail.com>
2022-09-06 16:52:12 +0800

    ci(.eslintrc.js): code style

--- packages/pkg1/src/index.ts
+++ packages/pkg1/src/index.ts
@@ -1,5 +1,11 @@
-console.log("pkg1");
-export const pkgName = "pkg1";
+console.log('pkg1');
+export const pkgName = 'pkg1';
 export default {
   pkgName
 };
+
+const a = (): void => {
+  console.log('dddddddddd');
+};
+
+console.log(a());
--- packages/pkg2/src/index.ts
+++ packages/pkg2/src/index.ts
@@ -1,5 +1,14 @@
-console.log("pkg2");
-export const pkgName = "pkg2";
+console.log('pkg2');
+export const pkgName = 'pkg2';
+
 export default {
   pkgName
 };
+
+const a = (): string => {
+  console.log('pkg2');
+
+  return 'pkg2';
+};
+
+console.log(a());
